868ededded8725c0384f5e34889b34aa60adaeaf
ammoblty <amittal@moblty.com>
2019-11-15 21:18:53 +0530

    date fix

--- instacar-task-api/src/config/seeds/user.js
+++ instacar-task-api/src/config/seeds/user.js
@@ -3,16 +3,30 @@
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 
-let userData = {
-    name: 'Abhishek Mittal',
-    email: 'abhsihekmittaloffice@gmal.com',
-    password: '1234',
-    phone: '9872387793'
-}
+let userData = [
+    {
+        fullName: 'Abhishek Mittal',
+        email: 'abhsihekmittaloffice@gmal.com',
+        password: '1234',
+        phone: '9872387793'
+    },
+    {
+        fullName: 'A Real Traveler',
+        email: 'art@aol.com',
+        password: '1234',
+        phone: '999998888'
+    },
+    {
+        fullName: 'Koi bohot bara naam',
+        email: 'kbbn@gmal.com',
+        password: '1234',
+        phone: '111111122'
+    },
+]
 
 module.exports = {
-    seed: () => {
-        let user = new User(userData);
+    seed: async () => {
+        let user = await User.insertMany(userData);
         return user.save()
             .then((user) => {
                 console.log('User::Admin seeded!');
